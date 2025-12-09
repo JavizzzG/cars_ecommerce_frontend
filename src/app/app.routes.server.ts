a5f8a3e0a00344d066524648766a3f3e546c1707
JavizzzG <javierstivengalvis2008@gmail.com>
2025-12-08 23:44:16 -0500

    Cambio en el SSR para una ruta de details

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,13 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: "car-detail/:id",
+    renderMode: RenderMode.Server
+  },
+  
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
+  
 ];
